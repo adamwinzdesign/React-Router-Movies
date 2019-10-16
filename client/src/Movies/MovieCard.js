4fb15b2c3f7ce6b34d27d23d3280374faf440abe
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-16 15:38:10 -0500

    finished routes

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props.movie;
+
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-metascore">
+        Metascore: {metascore}
+      </div>
+      <div className="movie-director">
+        Director: {director}
+      </div>
+      <div><h2>Actors:</h2></div>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+        <Link exact to={`/movies/${movie.id}`}>
+        <div className="saved-movie">
+          <span>{movie.title}</span>
+        </div>
+      </Link>
     ))}
-    <div className="home-button">Home</div>
+    <Link to="/">
+      <div className="home-button">Home</div>
+    </Link>
   </div>
 );
 
